96c4d11a3456589e656558f4518eb7de1604182c
AjithNarayan-V <ajithnarayan004@gmail.com>
2024-04-28 14:57:32 +0530

    first commit

---
+++ first-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar.jsx"
+import Body from "./components/body.jsx"
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+     
+    </>
+  )
+}
+
+export default App
